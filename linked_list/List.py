c539f3f5f70d638717f1c19c717df3cd371b09ea
hmigl <88746406+hmigl@users.noreply.github.com>
2023-09-14 10:48:45 -0300

    gottem

---
+++ linked_list/List.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, key: int) -> None:
+        self.key: int = key
+        self.next: Node | None = None
+
+    def get_key(self) -> int:
+        return self.key
+
+
+class List:
+    def __init__(self) -> None:
+        self.head: Node | None = None
+
+    def is_empty(self) -> bool:
+        return self.head is None
+
+    def search(self, x: int) -> bool:
+        curr: Node | None = self.head
+        while (curr is not None) and (curr.get_key() != x):
+            curr = curr.next
+        return curr is not None
+
+    def insert_back(self, x: int) -> bool:
+        if self.search(x):
+            return False
+        new: Node = Node(x)
+        if self.is_empty():
+            self.head = new
+        else:
+            curr: Node = self.head
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = new
+        return True
+
+    def insert_front(self, x: int) -> bool:
+        if self.search(x):
+            return False
+        new: Node = Node(x)
+        new.next = self.head
+        self.head = new
+        return True
+
+    def remove(self, x: int) -> None:
+        if not self.head:
+            return
+        if self.head.get_key() == x:
+            self.head = self.head.next
+            return
+        curr: Node = self.head
+        while curr.next is not None:
+            if curr.next.get_key() == x:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+
+    def print(self) -> None:
+        curr: Node | None = self.head
+        while curr is not None:
+            print(curr.get_key())
+            curr = curr.next
