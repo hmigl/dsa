61cd0959c73412d2d7cbc67d43ecf88088fe7c39
hmigl <88746406+hmigl@users.noreply.github.com>
2023-09-21 12:58:59 -0300

    refactor: change top/bottom to front/back

--- queue/QueueV2.py
+++ queue/QueueV2.py
@@ -6,34 +6,34 @@ class Node:
 
 class QueueV2:
     def __init__(self) -> None:
-        self.top: Node | None = None
-        self.bottom: Node | None = None
+        self.front: Node | None = None
+        self.back: Node | None = None
 
     def is_empty(self) -> bool:
-        return self.top is None
+        return self.front is None
 
     def enqueue(self, x: int) -> bool:
         node: Node = Node(x)
 
         if self.is_empty():
-            self.top = node
+            self.front = node
         else:
-            self.bottom.next = node
-        self.bottom = node
+            self.back.next = node
+        self.back = node
         return True
 
     def dequeue(self) -> tuple[bool, Node | None]:
-        node: Node | None = self.top
+        node: Node | None = self.front
         if node is None:
             return (False, None)
 
-        self.top = self.top.next
+        self.front = self.front.next
         if self.is_empty():
-            self.bottom = None
+            self.back = None
         return (True, node)
 
     def print(self) -> None:
-        curr: Node | None = self.top
+        curr: Node | None = self.front
         while curr is not None:
             print(curr.key, end=" ")
             curr = curr.next
