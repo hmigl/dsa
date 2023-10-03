06ca2131400bb7a4ed3460cce9641e23d9175267
hmigl <88746406+hmigl@users.noreply.github.com>
2023-10-03 12:16:00 -0300

    feat: verify if there's already a certain...
    ...word at the list

--- trab1/src/LinkedList.cpp
+++ trab1/src/LinkedList.cpp
@@ -11,3 +11,13 @@ LinkedList::~LinkedList() {
 }
 
 void LinkedList::insert(const std::string &word) {}
+bool LinkedList::find(const std::string &word) {
+  Node *curr = this->head;
+  while (curr) {
+    if (curr->word.compare(word) == 0) {
+      return true;
+    }
+    curr = curr->next4;
+  }
+  return false;
+}
