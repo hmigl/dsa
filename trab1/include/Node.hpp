5202ebe9f9bed3207dd4c68a47e727b961dd62b9
hmigl <88746406+hmigl@users.noreply.github.com>
2023-10-07 21:25:37 -0300

    refactor: transform Node into struct

--- trab1/include/Node.hpp
+++ trab1/include/Node.hpp
@@ -2,15 +2,10 @@
 
 #include <string>
 
-class Node {
- public:
-  Node();
-  Node(const std::string &);
-  Node(const Node &);
-  Node &operator=(const Node &) = default;
-  ~Node();
-
+struct Node {
   std::string word;
   Node *next;
   Node *next4;
+
+  Node(const std::string &word) : word(word), next(nullptr), next4(nullptr) {}
 };
