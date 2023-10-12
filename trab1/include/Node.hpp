67463839a7154432c3991cf1c3bd11832b97ed66
hmigl <88746406+hmigl@users.noreply.github.com>
2023-10-12 17:11:16 -0300

    docs: add documentation for 'Node'

--- trab1/include/Node.hpp
+++ trab1/include/Node.hpp
@@ -2,10 +2,14 @@
 
 #include <string>
 
+/**
+ * @struct Node
+ * @brief Estrutura de nó para armazenar palavras em listas encadeadas
+ */
 struct Node {
-  std::string word;
-  Node *nextInBasicList;
-  Node *crossListNext;
+  std::string word;      /**< A palavra armazenada no nó */
+  Node *nextInBasicList; /**< Ponteiro para o próximo nó na lista básica */
+  Node *crossListNext;   /**< Ponteiro para o próximo nó na 'lista 4' */
 
   Node(const std::string &word)
       : word(word), nextInBasicList(nullptr), crossListNext(nullptr) {}
